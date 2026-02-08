ff91071ff5f3bc246f6e0f3e6fd9d7a358c4ba6d
Vladimir <vladimir@example.com>
2026-02-08 22:51:34 +0300

    fix vercel config for site

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/New-cnc-site' : '',
+  // output: 'standalone',
+  // basePath: '/New-cnc-site', // Not needed for root domain/IP
   images: {
     unoptimized: true,
   },
